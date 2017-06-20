9bf0511b8e5faf7590ce7922d98875f972aab510
robertxm <hhong@iwstech.com>
2017-06-20 13:23:16 +0800

    add color button
    add color button

--- src/pages/imageeditor/imgeditor.ts
+++ src/pages/imageeditor/imgeditor.ts
@@ -15,6 +15,7 @@ export class ImgeditorPage {
 	canvas: any;
 	context: any;
 	imageC: any;
+	drawcolor: string;
 	constructor( public navCtrl: NavController, public alertCtrl: AlertController,public params: NavParams) {
 		this.images = [];
 		this.images = this.params.get('img');
@@ -27,6 +28,7 @@ export class ImgeditorPage {
 				elements[key].style.display = 'none';
 			});
 		}
+		this.drawcolor = '#0000ff';
 		this.editclick(this.params.get('imgdata'));
 
 	}
@@ -39,7 +41,7 @@ export class ImgeditorPage {
 		this.savetax = true;
 		var image = new Image();
 		image.onload = val => {
-			document.getElementById("canvasEditor").style.display = "";
+			//document.getElementById("canvasEditor").style.display = "";
 			this.canvas = document.getElementById("myCanvasEditor");
 			//this.canvas.width = 1;
 			//this.canvas.height = 1;
@@ -47,7 +49,7 @@ export class ImgeditorPage {
 			this.context.lineWidth = 3;
 			this.context.lineJoin = 'round';
 			this.context.lineCap = 'round';
-			this.context.strokeStyle = '#ff0000';
+			this.context.strokeStyle = this.drawcolor;
 			var mouse: any;
 			var mouses: Array<any>;
 			mouse = { x: 0, y: 0 };
@@ -65,9 +67,9 @@ export class ImgeditorPage {
 				mouses.push({ x: mouse.x, y: mouse.y });
 			}, false);
 
-			this.canvas.addEventListener('touchstart', (event) => {
-				this.context.strokeStyle = '#ff0000';
-				//this.context.beginPath();					
+			this.canvas.addEventListener('touchstart', (event) => {				
+				this.context.strokeStyle = this.drawcolor;					
+			    this.context.beginPath();				
 				mouse = { x: event.changedTouches[0].clientX - this.canvas.getBoundingClientRect().left, y: event.changedTouches[0].clientY - this.canvas.getBoundingClientRect().top };
 				this.context.moveTo(mouse.x, mouse.y);
 				//console.log(mouses);
@@ -83,11 +85,12 @@ export class ImgeditorPage {
 					this.context.lineTo(mouse.x, mouse.y);
 					this.context.stroke();
 				}, false);
+				this.mousestouch.push(this.context.strokeStyle);
 				this.mousestouch.push(mouses);
 				mouses = [];
 			}, false);
 			this.canvas.width = window.innerWidth;//330;//window.innerWidth;// image.width;
-			this.canvas.height = window.innerHeight - 60;//430;//window.innerHeight;//image.height-20;
+			this.canvas.height = window.innerHeight - 70;//430;//window.innerHeight;//image.height-20;
 			this.context.drawImage(image, 0, 0, this.canvas.width, this.canvas.height);
 			this.context.save();
 		}
@@ -112,20 +115,28 @@ export class ImgeditorPage {
 		this.images.push(newsrc);//'data:image/jpeg;base64,' + imageData);										
 		this.savetax = false;
 		this.context.clearRect(0, 0, this.context.width, this.context.height);
-		document.getElementById("canvasEditor").style.display = "none";
+		//document.getElementById("canvasEditor").style.display = "none";
 	}
-
+    blueclick(){
+		this.drawcolor = '#0000ff';
+		//this.context.strokeStyle = this.drawcolor;
+	}
+	redclick(){
+		this.drawcolor = '#ff0000';
+		//this.context.strokeStyle = this.drawcolor;
+	}
 	redrawclick() {
 		this.context.clearRect(0, 0, this.context.width, this.context.height);
 		this.context.drawImage(this.imageC, 0, 0, this.canvas.width, this.canvas.height);
 		this.mousestouch.pop();
+		this.mousestouch.pop();
 		var mouse: any;
 		var mouses: Array<any>;
-		mouses = [];
-		for (var i = 0; i < this.mousestouch.length; i++) {
-			mouses = this.mousestouch[i];
+		mouses = [];		
+		for (var i = 0; i < this.mousestouch.length; i+=2) {			
+			mouses = this.mousestouch[i+1];
 			mouse = mouses[0];
-			this.context.strokeStyle = '#ff0000';
+			this.context.strokeStyle = this.mousestouch[i];
 			this.context.moveTo(mouse.x, mouse.y);
 			this.context.beginPath();
 			for (var j = 1; j < mouses.length; j++) {
