df0d9d252ef11fdf3f102546262cae23a99db0ee
Robin <fdzhong@iwstech.com>
2017-07-03 16:29:52 +0800

    合并App.Module.ts (By Robin)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,19 @@ import { HttpModule } from '@angular/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
-
+import { MyinfoPage } from '../pages/myinfo/myinfo';
+import { MysettingsPage } from '../pages/mysettings/mysettings';
 import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
+import { LoginPage } from '../pages/login/login';
 import { TabsPage } from '../pages/tabs/tabs';
 import { FloorPage } from '../pages/floor/Floor';
 import { RoomPage } from '../pages/room/room';
 import { IssuePage } from '../pages/issue/issue';
 import { IssueviewPage } from '../pages/issueview/issueview';
+import { ReceiptPage } from '../pages/receipt/receipt';
+import { RejectPage } from '../pages/reject/reject';
 import { LocalStorage } from '../providers/local-storage';
 import { FormalinspectionPage } from '../pages/formalinspection/formalinspection';
 import { OpeninspectionPage } from '../pages/openinspection/openinspection';
@@ -21,9 +25,12 @@ import { HttpService } from '../providers/HttpService';
 import { initBaseDB } from '../providers/initBaseDB';
 //import { ImgeditorPage } from '../pages/imageeditor/imgeditor';
 import { ShowimgPage } from '../pages/imageeditor/showimg';
+import { ChangePWPage } from '../pages/changepw/changepw';
+import { MyPage } from '../pages/mypage/mypage';
+import { MyTeamPage } from '../pages/myteam/myteam';
 import { ImageEditorModal } from '../pages/imageeditor/imageeditormodal';
-import { ReceiptPage } from '../pages/receipt/receipt';
-import { RejectPage } from '../pages/reject/reject';
+import { AddphonecontactsPage } from '../pages/addphonecontacts/addphonecontacts';
+import { AddmembermanualPage } from '../pages/addmembermanual/addmembermanual';
 import { Signature } from '../pages/signature/signature';
 import { Rejecthistory } from '../pages/rejecthistory/rejecthistory';
 
@@ -36,7 +43,9 @@ import { IonicStorageModule } from '@ionic/storage';
 import { NativeService } from '../providers/nativeservice';
 import { Network } from '@ionic-native/network';
 import { Dialogs } from '@ionic-native/dialogs';
-import { SignaturePadModule } from 'angular2-signaturepad';
+import { Toast } from '@ionic-native/toast';
+import { Contacts } from '@ionic-native/contacts';
+import { SignaturePadModule } from 'angular2-signaturepad'
 
 
 @NgModule({
@@ -45,6 +54,15 @@ import { SignaturePadModule } from 'angular2-signaturepad';
         AboutPage,
         ContactPage,
         HomePage,
+        LoginPage,
+        MyPage,
+        MyinfoPage,
+        MysettingsPage,
+        ChangePWPage,
+        MyTeamPage,
+        AddphonecontactsPage,
+        AddmembermanualPage,
+
         FloorPage,
         RoomPage,
         IssuePage,
@@ -68,7 +86,7 @@ import { SignaturePadModule } from 'angular2-signaturepad';
             tabsHideOnSubPages: 'true'         //隐藏全部子页面tabs
         }),
         IonicStorageModule.forRoot(),
-        SignaturePadModule 
+        SignaturePadModule
     ],
     bootstrap: [IonicApp],
     entryComponents: [
@@ -78,6 +96,14 @@ import { SignaturePadModule } from 'angular2-signaturepad';
         HomePage,
         FloorPage,
         RoomPage,
+        LoginPage,
+        MyPage,
+        MyinfoPage,
+        MysettingsPage,
+        ChangePWPage,
+        MyTeamPage,
+        AddphonecontactsPage,
+        AddmembermanualPage,
         IssuePage,
         IssueviewPage,
         ImageEditorModal,
